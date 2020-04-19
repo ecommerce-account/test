b3653bcb3bd8297cdfee3d36611c94b0e8e615d8
MF <ecommerce_acount_1234@hotmail.com>
2020-04-19 19:14:13 +0900

    flaskテスト

---
+++ flask_app.py
@@ -0,0 +1,31 @@
+import stripe
+from flask import render_template, Flask, request
+
+app = Flask(__name__)
+
+STRIPE_SECRET_KEY="sk_test_si9qlwJI4IVNf29PbhwA6sKA00KWtNmD1B"
+
+stripe.api_key = STRIPE_SECRET_KEY
+@app.route('/')
+def hello_world():
+    return render_template('item.html')
+
+@app.route('/charge', methods=['POST'])
+def charge():
+    user_email = request.form["email"]
+    try:
+        amount = 70
+        customer = stripe.Customer.create(
+            email=user_email,
+            source=request.form['stripeToken']
+        )
+        stripe.Charge.create(
+            customer=customer.id,
+            amount=amount,
+            currency='jpy',
+            description='ÅZÅZè§ìXÇÃÇ®Ç∑Ç∑ÇﬂïiÅiPythonAnywhereÅj',
+            receipt_email=user_email,
+        )
+        return render_template('charge.html', amount=amount)
+    except stripe.error.StripeError:
+        return render_template('error.html')
